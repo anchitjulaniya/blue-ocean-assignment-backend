0f35a2cfec594cac3408c2eb767527aa36c5e0b1
anchit julaniya <anchitjulaniyaofficial@gmail.com>
2026-02-01 11:07:58 +0530

    handling deployment

--- server.js
+++ server.js
@@ -14,17 +14,17 @@ env.config();
 app.use(cors());
 app.use(express.json());
 
-db();
-
 app.use('/api/v1/category', categoryRoutes);
 app.use('/api/v1/subcategory', subCategoryRoutes);
 app.use('/api/v1/course', courseRoutes);
 
-app.use(errorMiddleware);
-
 app.get('/health', (req, res) => {
   res.status(StatusCodes.OK).json({ status: 'OK' });
 });
+
+app.use(errorMiddleware);
+
+db();
 
 app.listen(PORT, '0.0.0.0', () => {
     console.log("Server is running on port: ", PORT);
